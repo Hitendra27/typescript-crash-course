5eb19616e031723bdfcebb5a96929598be52e765
Hitendra Quessou <sanjay_quessou@yahoo.co.uk>
2023-03-14 14:11:44 +0000

    added Enums and Classes in TS.

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,71 @@
 "use strict";
+// Basic Types
 let id = 5;
+let company = 'Traversy Media';
+let isPublished = true;
+let x = 'Hello';
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'Hello'];
+// Tuple
+let person = [1, 'Brad', false];
+// Tuple Array
+let employee;
+employee = [
+    [1, 'Brad'],
+    [2, 'Travis'],
+    [3, 'Phil'],
+];
+// Union
+let pid;
+pid = '22';
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+const user = {
+    id: 1,
+    name: 'John',
+};
+// Type Assertion
+let cid = 1;
+//let customerId = <number>cid
+let customerId = cid;
+// Functions
+function addNum(x, y) {
+    return x + y;
+}
+// Void
+function log(message) {
+    console.log(message);
+}
+const user1 = {
+    id: 1,
+    name: 'John',
+};
+const add = (x, y) => x + y;
+const sub = (x, y) => x - y;
+// Classes
+class Person {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    register() {
+        return `${this.name} is now registered`;
+    }
+}
+const brad = new Person(1, 'Brad Traversy');
+const mike = new Person(2, 'Mike Jordan');
+console.log(brad.register());
+console.log(brad, mike);
